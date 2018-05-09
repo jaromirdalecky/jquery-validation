9e4b078eb70dce87c5f673521b095f34d81d83b1
Jaromir Dalecky <jaromir@harvestdp.com>
2018-05-09 10:26:32 +1000

    Core: Fix invalid value trimming

--- src/core.js
+++ src/core.js
@@ -1373,7 +1373,7 @@ $.extend( $.validator, {
 			if ( this.checkable( element ) ) {
 				return this.getLength( value, element ) > 0;
 			}
-			return value !== undefined && value !== null && $.trim(value.length) > 0;
+			return value !== undefined && value !== null && $.trim( value ).length > 0;
 		},
 
 		// https://jqueryvalidation.org/email-method/
